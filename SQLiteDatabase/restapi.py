f0420d280531a5c7616a8b2d072896da0912915f
cadenmf24 <cadenmf24@gmail.com>
2024-04-16 15:09:24 -0400

    it does in fact get

--- SQLiteDatabase/restapi.py
+++ SQLiteDatabase/restapi.py
@@ -1,9 +1,11 @@
 from dbUtil import *
 
 def getCyberAssets():
-    result = exec_get_all("SELECT name, type, os, serialNumber FROM cyberassets inner join serialnumber ON cyberassets.id = setialnumber.cyberassets_id")
+    result = exec_get_all("SELECT name, type, os, serialNumber FROM cyberassets inner join serialnumbers ON cyberassets_id = cyberassets.id")
     return result
     
+    result = exec_get_all('select courses.id, c_number, c_title, c_details, dept_id, department.name, college.name FROM courses inner join department ON department.id = courses.dept_id inner join college ON college.id = department.college_id order by id asc')
+
 
 def deleteCyberAssets(primaryid):
     exec_commit('DELETE FROM cyberassets WHERE id = %s', [primaryid])
