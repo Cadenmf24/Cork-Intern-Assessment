f4277a25fa3661da954dff845e1d4e1aabcd8b3a
cadenmf24 <cadenmf24@gmail.com>
2024-04-16 14:44:02 -0400

    created a flask api, will check what error codes are supposed to go there

--- SQLiteDatabase/flaskAPI.py
+++ SQLiteDatabase/flaskAPI.py
@@ -5,4 +5,31 @@ from flask_restful import reqparse
 
 from dbUtil import *
 
-# from 
+from restapi import *
+
+
+class Getcyberassets(Resource):
+    
+    def get(self):
+        
+        cyberAssetsList = []
+        cyberAssets = getCyberAssets()
+        
+        for i in cyberAssets:
+            cyberAssetsList.append(i)
+            
+        return (cyberAssetsList, 201)
+
+class Deletecyberassets(Resource):
+    
+    def delete(self, primaryid):
+        
+        print(primaryid)
+        if (primaryid != None):
+            deleteCyberAssets
+            return ("Cyber Asset Deleted", 202)
+        
+        else:
+            return ("ERROR, ASSET DOESN'T EXIST", 406)
+    
+    
--- SQLiteDatabase/server.py
+++ SQLiteDatabase/server.py
@@ -3,4 +3,13 @@ from flask_restful import Api
 from flask_cors import CORS
 
 from dbUtil import *
-# from 
+from flaskAPI import *
+
+app = Flask(__name__)
+CORS(app)
+
+api = Api(app)
+
+
+api.add_resource(getCyberAssets, "/cyberassets")
+api.add_resource(deleteCyberAssets, "/cyberassets")
